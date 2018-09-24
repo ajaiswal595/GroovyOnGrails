fc5dd0d77faf71c44fd74b067b312338efaa5eef
ajaiswal595 <ajaiswal595@gmail.com>
2018-09-24 14:35:35 +0530

    inList [Purchase,Reward]

--- Rewards/grails-app/domain/rewards/Award.groovy
+++ Rewards/grails-app/domain/rewards/Award.groovy
@@ -9,5 +9,6 @@ class Award {
     static hasMany = [orderItems:OrderItem]
 
     static constraints = {
+        type(inList: ["Purchase","Reward"] )    //it will create a drop down list to select either Purchase or Reward for type attribute in domain.
     }
 }
