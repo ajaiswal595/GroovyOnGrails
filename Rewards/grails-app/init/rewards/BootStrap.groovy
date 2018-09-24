f407711af725ba3aab524c1c5f3c5fab96cc321f
ajaiswal595 <ajaiswal595@gmail.com>
2018-09-24 14:52:34 +0530

    prepopulate data when bootstaping the application

--- Rewards/grails-app/init/rewards/BootStrap.groovy
+++ Rewards/grails-app/init/rewards/BootStrap.groovy
@@ -1,8 +1,12 @@
 package rewards
 
+//BootStrap is something which get Executed when application server start.
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Product(name:"Morning Blend",sku:"MB01",price: 14.95).save()         //here i'm prepopulating the data in data base to get rid of data insertion when server restarted.
+        new rewards.Product(name:"Dark Roast",sku:"DK01",price: 12.95).save()
     }
     def destroy = {
     }
