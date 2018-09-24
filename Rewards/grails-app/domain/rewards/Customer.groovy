fb7908041a5fcfa682c3562784f58e23a30ac55f
ajaiswal595 <ajaiswal595@gmail.com>
2018-09-24 14:25:09 +0530

    added email validation

--- Rewards/grails-app/domain/rewards/Customer.groovy
+++ Rewards/grails-app/domain/rewards/Customer.groovy
@@ -15,10 +15,12 @@ class Customer {
         //the order you provide here same will reflect in html
 
         // the field having nullable : true are not mandatory field
+
+         //you can't use size if your field is nullable for ex :  firstName(nullable: true,size:1..15)    it's wrong statement
         phone()
-        firstName(nullable: true    )
-        lastName(nullable: true)
-        email(nullable: true)
-        totalPoints()
+        firstName(nullable: true,size:1..15)  // it's working in grails 3.x but in 2.x it don't as per udemy trainer
+        lastName(nullable: true,maxSize: 30)
+        email(nullable: true, email: true)    // email:true is validation on the text you will enter in text box , it will accept valid email.
+        totalPoints(nullable: true,max: 10)
     }
 }
