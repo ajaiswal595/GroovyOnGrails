15d776b718876693b91182990f3878d1497176ca
ajaiswal595 <ajaiswal595@gmail.com>
2018-09-25 18:19:51 +0530

    added date and performed add operation

--- Rewards/grails-app/controllers/rewards/WhiteboardController.groovy
+++ Rewards/grails-app/controllers/rewards/WhiteboardController.groovy
@@ -9,5 +9,14 @@ class WhiteboardController {
     def variables(){
         def myTotal = 1
         render("Total: "+myTotal)   //render send data to view layer
+
+        def firstName ="Ajay"
+        render("</br>Name : "+firstName)
+
+        def today = new Date()
+        render("<br> Today is "+today.getDate())
+
+       /*  today = today+1    // we can perform add numbers so it will tell you the day by calculating leap year as well
+        render("<br> Today is "+today.getDate())*/
     }
 }
