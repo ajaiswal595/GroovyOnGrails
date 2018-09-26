2ede99f2ef63403b17ed6484eecbe4daf3910331
ajaiswal595 <ajaiswal595@gmail.com>
2018-09-26 21:19:50 +0530

    added sorting , order by m pagination

--- Rewards/grails-app/controllers/rewards/CustomerController.groovy
+++ Rewards/grails-app/controllers/rewards/CustomerController.groovy
@@ -17,7 +17,8 @@ class CustomerController {
     def lookup(){
         //customerInstance -> name of the controller you are calling form suffix with Instance   -> It's not a hard and fast rule
         //Customer.list() -> It will qurey the customer domain and return the whole list of customer
-        def customerInstance = Customer.list()
+       //sort by lastName , order asc / desc , pagination max per page , offset add from current list shown in pagination
+        def customerInstance = Customer.list(sort:"lastName",orders: "asc",max:5,offset:5)
 
         //here we are returning the map which lookup.gsp will use to render the customer list.
         [customerInstanceList : customerInstance]
