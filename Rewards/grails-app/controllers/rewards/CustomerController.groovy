9df1a60780676c7719d7e02421bcf6a0e1a93507
ajaiswal595 <ajaiswal595@gmail.com>
2018-09-26 21:32:17 +0530

    added funcality to read param from url and fetch the data

--- Rewards/grails-app/controllers/rewards/CustomerController.groovy
+++ Rewards/grails-app/controllers/rewards/CustomerController.groovy
@@ -18,7 +18,18 @@ class CustomerController {
         //customerInstance -> name of the controller you are calling form suffix with Instance   -> It's not a hard and fast rule
         //Customer.list() -> It will qurey the customer domain and return the whole list of customer
        //sort by lastName , order asc / desc , pagination max per page , offset add from current list shown in pagination
-        def customerInstance = Customer.list(sort:"lastName",orders: "asc",max:5,offset:5)
+
+        // def customerInstance = Customer.list(sort:"lastName",orders: "asc",max:5,offset:5)
+
+        //it will fetch the record of customer having totalPoint = 5.
+       // def customerInstance = Customer.findAllByTotalPoints(5,[sort:"lastName"])
+
+
+        //http://localhost:1515/customer/lookup/8015551212
+        //it will read the phone from url fetch the data accordingly.
+
+        def customerInstance  = Customer.findByPhone(params.id)
+
 
         //here we are returning the map which lookup.gsp will use to render the customer list.
         [customerInstanceList : customerInstance]
