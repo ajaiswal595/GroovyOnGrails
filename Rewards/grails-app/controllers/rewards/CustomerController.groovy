5ecbbf0558f4dbdc3307f337ae5312b15fa34389
ajaiswal595 <ajaiswal595@gmail.com>
2018-09-26 18:17:23 +0530

    added bootraping data and lookup.gsp and action to render customer damain list

--- Rewards/grails-app/controllers/rewards/CustomerController.groovy
+++ Rewards/grails-app/controllers/rewards/CustomerController.groovy
@@ -11,4 +11,16 @@ class CustomerController {
     def checkin() {
 
     }
+
+    //lookup will call the lookup.gsp
+
+    def lookup(){
+        //customerInstance -> name of the controller you are calling form suffix with Instance   -> It's not a hard and fast rule
+        //Customer.list() -> It will qurey the customer domain and return the whole list of customer
+        def customerInstance = Customer.list()
+
+        //here we are returning the map which lookup.gsp will use to render the customer list.
+        [customerInstanceList : customerInstance]
+
+    }
 }
